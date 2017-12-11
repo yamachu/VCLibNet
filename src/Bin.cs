3668c6463ab95c6b3d76593678714ac5a06352b1
Yusuke Yamada <yamachu.dev@gmail.com>
2017-12-11 13:45:25 +0900

    VCLibの検証用にラップする関数を追加

--- src/Bin.cs
+++ src/Bin.cs
@@ -75,5 +75,103 @@ namespace VCLibNet
 
             return y;
         }
+
+        public static float[] GetUserMcep(float[] spectrum)
+        {
+            IntPtr ptr_mcep = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(float)) * spectrum.Length);
+
+            FloatDefinitions.GetUserMcep(spectrum, spectrum.Length, ptr_mcep);
+
+            var mcep = new float[spectrum.Length];
+
+            Marshal.Copy(ptr_mcep, mcep, 0, mcep.Length);
+            Marshal.FreeHGlobal(ptr_mcep);
+
+            return mcep;
+        }
+
+        public static float[] GetCompensationWavForm(float[] spectrum, float[] userMcep, float[] targetMcep)
+        {
+            IntPtr ptr_y = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(float)) * spectrum.Length);
+
+            var y_length = FloatDefinitions.GetCompensationWavForm(spectrum, spectrum.Length, userMcep, targetMcep, userMcep.Length, ptr_y);
+
+            var y = new float[y_length];
+
+            Marshal.Copy(ptr_y, y, 0, y.Length);
+            Marshal.FreeHGlobal(ptr_y);
+
+            return y;
+        }
+
+        public static double[] GetUserMcep(double[] spectrum)
+        {
+            IntPtr ptr_mcep = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(double)) * spectrum.Length);
+
+            DoubleDefinitions.GetUserMcep(spectrum, spectrum.Length, ptr_mcep);
+
+            var mcep = new double[spectrum.Length];
+
+            Marshal.Copy(ptr_mcep, mcep, 0, mcep.Length);
+            Marshal.FreeHGlobal(ptr_mcep);
+
+            return mcep;
+        }
+
+        public static double[] GetCompensationWavForm(double[] spectrum, double[] userMcep, double[] targetMcep)
+        {
+            IntPtr ptr_y = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(double)) * spectrum.Length);
+
+            var y_length = DoubleDefinitions.GetCompensationWavForm(spectrum, spectrum.Length, userMcep, targetMcep, userMcep.Length, ptr_y);
+
+            var y = new double[y_length];
+
+            Marshal.Copy(ptr_y, y, 0, y.Length);
+            Marshal.FreeHGlobal(ptr_y);
+
+            return y;
+        }
+
+        public static float[] Standardization1DArray(float[] source, int dim)
+        {
+            IntPtr ptr_result = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(float)) * source.Length);
+
+            FloatDefinitions.Standardization1DArray(source, source.Length, dim, ptr_result);
+
+            var result = new float[source.Length];
+
+            Marshal.Copy(ptr_result, result, 0, result.Length);
+            Marshal.FreeHGlobal(ptr_result);
+
+            return result;
+        }
+
+        public static float[] UnStandardization1DArray(float[] source, int dim, float[] means, float[] sds)
+        {
+            IntPtr ptr_result = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(float)) * source.Length);
+
+            FloatDefinitions.UnStandardization1DArray(source, source.Length, dim, means, sds, ptr_result);
+
+            var result = new float[source.Length];
+
+            Marshal.Copy(ptr_result, result, 0, result.Length);
+            Marshal.FreeHGlobal(ptr_result);
+            
+            return result;
+        }
+
+        public static float[] VarianceCompensation(float[] source, int dim, float[] coef)
+        {
+            IntPtr ptr_result = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(float)) * source.Length);
+
+            FloatDefinitions.VarianceCompensation(source, source.Length, dim, coef, ptr_result);
+
+            var result = new float[source.Length];
+
+            Marshal.Copy(ptr_result, result, 0, result.Length);
+            Marshal.FreeHGlobal(ptr_result);
+            
+            return result;
+        }
     }
 }
--- src/BinDefinitions.cs
+++ src/BinDefinitions.cs
@@ -18,6 +18,21 @@ namespace VCLibNet.Definition.Bin
         [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 3)]double[] mcep, int mcep_length,
             int order, double alpha, int frame_period, int i_period, int pade,
             int is_tranpose, int is_invrese, int is_coef_b, int is_without_gain, [In][Out] IntPtr y);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GetUserMcep([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]double[] spectrum, int sp_length, [In][Out] IntPtr mcep);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetCompensationWavForm([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]double[] wavform, int wavform_length, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)]double[] userMcep, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)]double[] targetMcep, int mcep_length, [In][Out] IntPtr y);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Standardization1DArray([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] source, int source_length, int dim, [In][Out] IntPtr result);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void UnStandardization1DArray([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] source, int source_length, int dim, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)]float[] means, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)]float[] sds, [In][Out] IntPtr result);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void VarianceCompensation([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] source, int source_length, int dim, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)]float[] coef, [In][Out] IntPtr result);
     }
 
     internal static class FloatDefinitions
@@ -35,5 +50,20 @@ namespace VCLibNet.Definition.Bin
         [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 3)]float[] mcep, int mcep_length,
             int order, double alpha, int frame_period, int i_period, int pade,
             int is_tranpose, int is_invrese, int is_coef_b, int is_without_gain, [In][Out] IntPtr y);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GetUserMcep([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] spectrum, int sp_length, [In][Out] IntPtr mcep);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetCompensationWavForm([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] wavform, int wavform_length, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)]float[] userMcep, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 4)]float[] targetMcep, int mcep_length, [In][Out] IntPtr y);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Standardization1DArray([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] source, int source_length, int dim, [In][Out] IntPtr result);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void UnStandardization1DArray([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] source, int source_length, int dim, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)]float[] means, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)]float[] sds, [In][Out] IntPtr result);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void VarianceCompensation([In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 1)]float[] source, int source_length, int dim, [In][MarshalAs(UnmanagedType.LPArray, SizeParamIndex = 2)]float[] coef, [In][Out] IntPtr result);
     }
 }
